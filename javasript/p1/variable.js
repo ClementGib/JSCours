13bfbee3fc6d918cd70aeb70b7dbaad3f6ccf7c0
CodingClem <c.gibertpro@gmail.com>
2022-07-02 17:19:47 +0200

    exemple

--- javasript/p1/variable.js
+++ javasript/p1/variable.js
@@ -1,3 +1,5 @@
+
+
 /// ???
 var chiffre = 10;
 console.log(chiffre);
@@ -8,3 +10,65 @@ console.log(chiffre);
 
 // ???
 console.log(chiffre + 10);
+//chiffre ???
+
+chiffre = chiffre + 10;
+console.log(chiffre);
+
+chiffre += 10;
+console.log(chiffre);
+
+chiffre1 = 50;
+chiffre2 = 44;
+
+chiffre1 == chiffre2;
+
+chiffre2 += 10;
+chiffre2 -= 10;
+chiffre2 *= 10;
+chiffre2 /= 10;
+chiffre2 %= 10;
+
+//x = 1 2 3 4 5 6 7 8 9 10
+
+if (x % 3 == 0) {
+    console.log("Cest un multiple de 3")  
+} else {
+    console.log("Cest pas un multiple de 3")
+}
+
+chiffre1 == chiffre2;
+chiffre1 != chiffre2;
+
+chiffre3 = 12.5;
+chiffre4 = 12;
+
+name = "tanguy";
+text = "Salut";
+
+text2 = 'salut';
+
+
+
+
+vivant = false;
+vivant = true;
+
+vivant = "Ouais";
+
+text1 = null;   
+text2 = "";
+
+
+number1 = 10;
+number2 = 20;
+number2 != number1; // true
+number2 == number1; // false
+
+value1 = undefined;
+value2 = null;
+
+value3 = console.log("COucou");
+
+
+//if (name != null && surname != null)
