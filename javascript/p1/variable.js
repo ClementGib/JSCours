0a26ba7047d8430722b7ac6043d0aef2049d3b0e
CodingClem <c.gibertpro@gmail.com>
2022-07-10 22:45:04 +0200

    correction variable.js

--- javascript/p1/variable.js
+++ javascript/p1/variable.js
@@ -6,37 +6,37 @@
 //1 SIMPLE
 
 chiffre = 10;
-/// résultat: 10
+/// résultat: 10 🟢
 console.log(chiffre);
 
 chiffre = 20;
-/// résultat: 20
+/// résultat: 20 🟢
 console.log(chiffre);
 
-/// résultat: 30
+/// résultat: 30 🟢
 console.log(chiffre + 10);
 
-/// résultat: 20
+/// résultat: 20 🟢
 console.log(chiffre);
 
 chiffre += 10;
-/// résultat: 30
+/// résultat: 30 🟢
 console.log(chiffre);
 
 chiffre -= 10;
-/// résultat: 20
+/// résultat: 20 🟢
 console.log(chiffre);
 
 chiffre *= 2;
-/// résultat: 40
+/// résultat: 40 🟢
 console.log(chiffre);
 
 chiffre /= 2;
-/// résultat: 20
+/// résultat: 20 🟢
 console.log(chiffre);
 
 chiffre %= 2;
-/// résultat: 0
+/// résultat: 0 🟢
 console.log(chiffre);
 
 //2 ASSIGNATION
@@ -44,66 +44,71 @@ console.log(chiffre);
 chiffre1 = 100;
 chiffre2 = 50;
 chiffre3 = chiffre1 + chiffre2;
-/// résultat: 150
+/// résultat: 150 🟢
 console.log(chiffre3);
 
 chiffre3 -= chiffre2;
-/// résultat: 100
+/// résultat: 100 🟢
 console.log(chiffre3);
 
 chiffre3 -= true;
-/// résultat: jsp je dirais NaN ou un truc qui veut dire error 
+/// réponse: jsp je dirais NaN ou un truc qui veut dire error  🔴
+/// résultat: 99, true est compté comme un 1
 console.log(chiffre3);
 
 chiffre3 += true;
-/// résultat: idem
+/// réponse: idem 🔴
+/// résultat: 100, true est compté comme un 1
 console.log(chiffre3);
 
 mot1 = "EZ";
 mot2 = "PZ";
-/// résultat: EZ.PZ
+/// résultat: EZ.PZ 🟢
 console.log(mot1 + "." + mot2);
 
 juste  = "True";
-/// résultat: truefalse mais ca me parait bizzare ou alors un truc comme true + false et puisque le false est en dernier c'est lui qui prends le dessus 
+/// réponse: truefalse mais ca me parait bizzare ou alors un truc comme true + false et puisque le false est en dernier c'est lui qui prends le dessus 
+/// résultat: Truefalse, les deux sont considéré comme un string
 console.log(juste + false);
 
-/// résultat: comme au dessus donc false qui prends le dessus maybe
+/// réponse: comme au dessus donc false qui prends le dessus maybe 🔴
+// résultat: 1, considéré comme des nombre 1 + 0
 console.log(true + false);
 
-/// résultat: true
+/// réponse: true 🔴
+// résultat: 1, considéré comme des nombre 1 + 1
 console.log(true + true);
 
 // Alors oui il peut y avoir des trucs bizarre mais il faut essayer de comprendre Pourquoi
 // Ca peut devenir très utile par la suite...
 
 age = 18;
-/// résultat: false
+/// résultat: false 🟢
 console.log(age > 18 );
 
-/// résultat: true
+/// résultat: true 🟢
 console.log(age == 18 );
 
-/// résultat: true
+/// résultat: true 🟢
 console.log(age == "18" );
 
-/// résultat: false
+/// résultat: false 🟢
 console.log(age === "18" );
 
 age = 60;
-/// résultat: true
+/// résultat: true 🟢
 console.log(age > 18 && age >= 60);
 
-/// résultat: true si j'ai bien compris le !age 
+/// résultat: true si j'ai bien compris le !age 🟢
 console.log(!age != 60);
 
 
 numero = 1.5;
-/// résultat: true
+/// résultat: true 🟢
 console.log(numero > 1 && numero < 2);
 
 numero = 1.5;
-/// résultat: true
+/// résultat: true 🟢
 console.log(numero < 1 || numero < 2);
 
 
@@ -111,7 +116,11 @@ console.log(numero < 1 || numero < 2);
 annee = 2022;
 age = 18;
 annee == 2022 && (annee - age > 2000) ? "Gen Z 👶🏻" : "Millennials 🦄";
-/// résultat: ????
+// résultat: "Gen Z 👶🏻"
+// annee == 2022 -> true
+// (annee - age > 2000) -> 2022 - 18 > 2000 -> true
+// true && true -> true -> "Gen Z 👶🏻"
+// si cetait false -> "Millennials 🦄"
 console.log(annee);
 
 
