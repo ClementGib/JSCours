88ac117a8f87242727d377d2288827c0280438c6
sslleeppyy <martangy02@gmail.com>
2022-07-10 16:14:52 +0200

    correction de l'exercice

--- javascript/p1/variable.js
+++ javascript/p1/variable.js
@@ -6,37 +6,37 @@
 //1 SIMPLE
 
 chiffre = 10;
-/// résultat: ????
+/// résultat: 10
 console.log(chiffre);
 
 chiffre = 20;
-/// résultat: ????
+/// résultat: 20
 console.log(chiffre);
 
-/// résultat: ????
+/// résultat: 30
 console.log(chiffre + 10);
 
-/// résultat: ????
+/// résultat: 20
 console.log(chiffre);
 
 chiffre += 10;
-/// résultat: ????
+/// résultat: 30
 console.log(chiffre);
 
 chiffre -= 10;
-/// résultat: ????
+/// résultat: 20
 console.log(chiffre);
 
 chiffre *= 2;
-/// résultat: ????
+/// résultat: 40
 console.log(chiffre);
 
 chiffre /= 2;
-/// résultat: ????
+/// résultat: 20
 console.log(chiffre);
 
 chiffre %= 2;
-/// résultat: ????
+/// résultat: 0
 console.log(chiffre);
 
 //2 ASSIGNATION
@@ -44,66 +44,66 @@ console.log(chiffre);
 chiffre1 = 100;
 chiffre2 = 50;
 chiffre3 = chiffre1 + chiffre2;
-/// résultat: ????
+/// résultat: 150
 console.log(chiffre3);
 
 chiffre3 -= chiffre2;
-/// résultat: ????
+/// résultat: 100
 console.log(chiffre3);
 
 chiffre3 -= true;
-/// résultat: ????
+/// résultat: jsp je dirais NaN ou un truc qui veut dire error 
 console.log(chiffre3);
 
 chiffre3 += true;
-/// résultat: ????
+/// résultat: idem
 console.log(chiffre3);
 
 mot1 = "EZ";
 mot2 = "PZ";
-/// résultat: ????
+/// résultat: EZ.PZ
 console.log(mot1 + "." + mot2);
 
 juste  = "True";
-/// résultat: ????
+/// résultat: truefalse mais ca me parait bizzare ou alors un truc comme true + false et puisque le false est en dernier c'est lui qui prends le dessus 
 console.log(juste + false);
 
-/// résultat: ????
+/// résultat: comme au dessus donc false qui prends le dessus maybe
 console.log(true + false);
 
-/// résultat: ????
+/// résultat: true
 console.log(true + true);
 
 // Alors oui il peut y avoir des trucs bizarre mais il faut essayer de comprendre Pourquoi
 // Ca peut devenir très utile par la suite...
 
 age = 18;
-/// résultat: ????
+/// résultat: false
 console.log(age > 18 );
 
-/// résultat: ????
+/// résultat: true
 console.log(age == 18 );
 
-/// résultat: ????
+/// résultat: true
 console.log(age == "18" );
 
-/// résultat: ????
+/// résultat: false
 console.log(age === "18" );
 
 age = 60;
-/// résultat: ????
+/// résultat: true
 console.log(age > 18 && age >= 60);
 
-/// résultat: ????
+/// résultat: true si j'ai bien compris le !age 
 console.log(!age != 60);
 
 
 numero = 1.5;
-/// résultat: ????
+/// résultat: true
 console.log(numero > 1 && numero < 2);
 
 numero = 1.5;
-/// résultat: ????
+/// résultat: true
 console.log(numero < 1 || numero < 2);
 
 
