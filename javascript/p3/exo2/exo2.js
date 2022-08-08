382456d22d5846cb2b2f05a19efb3acf0a90a450
CodingClem <c.gibertpro@gmail.com>
2022-08-09 00:13:40 +0200

    WIP exo2 functions

--- javascript/p3/exo2/exo2.js
+++ javascript/p3/exo2/exo2.js
@@ -1,27 +1,35 @@
 // Calculatrice de l'exo2 des conditions
 
+// do {
+//     let input = prompt("0")
+// } while(input != "exit")
 
-while
 
-var nombre1 = prompt("entrez le premier nombre")
-if(isNaN(nombre1)){
-    window.alert(nombre1 + " " + "n\'\est pas un nombre")
-}
-var operateur = prompt("entrez un operateur")
-var nombre2 = prompt("entrez le deuxieme nombre")
-if(isNaN(nombre2)){
-    window.alert(nombre2 + " " + "n\'\est pas un nombre")
-}
-switch(operateur){
-    case '+':
-    window.alert(parseInt(nombre1)+parseInt(nombre2))
-    break;
-    case '-':
-    window.alert(nombre1-nombre2)
-    break;
-    case 'x':
-    window.alert(nombre1*nombre2)
-    break;
-    case '/':
-    window.alert(nombre1/nombre2)
+
+// var nombre1 = prompt("entrez le premier nombre")
+// if(isNaN(nombre1)){
+//     window.alert(nombre1 + " " + "n\'\est pas un nombre")
+// }
+// var operateur = prompt("entrez un operateur")
+// var nombre2 = prompt("entrez le deuxieme nombre")
+// if(isNaN(nombre2)){
+//     window.alert(nombre2 + " " + "n\'\est pas un nombre")
+// }
+// switch(operateur){
+//     case '+':
+//     window.alert(parseInt(nombre1)+parseInt(nombre2))
+//     break;
+//     case '-':
+//     window.alert(nombre1-nombre2)
+//     break;
+//     case 'x':
+//     window.alert(nombre1*nombre2)
+//     break;
+//     case '/':
+//     window.alert(nombre1/nombre2)
+// }
+
+
+function display(value) {
+    document.getElementById("result").value += value;
 }
